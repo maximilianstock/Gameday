9137e46cebbcead9137e16d36c3e45aec03cf4ee
Maximilian Stock <maximilian@stock-family.com>
2024-12-04 11:24:15 +0100

    Initial Commit

---
+++ Gameday/GamedayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamedayApp.swift
+//  Gameday
+//
+//  Created by Maximilian Stock on 04.12.24.
+//
+
+import SwiftUI
+
+@main
+struct GamedayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
